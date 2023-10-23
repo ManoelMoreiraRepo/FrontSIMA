50133df35524ef32b3348458b6e60a683042210a
Matias Basilotta <matiasbasilotta@gmail.com>
2023-10-23 07:45:24 -0300

    Vista bolsa de trabajo armada

--- src/app/paginas/bolsaadmin/bolsaadmin.component.ts
+++ src/app/paginas/bolsaadmin/bolsaadmin.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class BolsaadminComponent {
   urlImagen : string = "./assets/img/Ellipse 99.png";
   urlImagenPostu:string = "./assets/img/postu.png";
-  lista = [
+  lista = [ //AGREGAR LOGICA PARA REUTIRLIZAR ESTA PANTALLA DEPENDIENDO DEL USUARIO.
     {
       img:"./assets/img/ecoklinTransparente.png",
       codigo:"1244",
@@ -24,4 +24,8 @@ export class BolsaadminComponent {
       url:"/postularse?id=1245"
     }
   ]
+
+  onFileChange=(event:any)=>{
+    console.log("Subida");
+  }
 }
