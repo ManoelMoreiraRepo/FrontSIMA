59bc3d9afcf51db7f31a135749afe07e3c0b8a0e
Matias Basilotta <matiasbasilotta@gmail.com>
2023-10-31 21:29:43 -0300

    Armado de vista indumentaria y solucion error de dependecias

--- src/app/paginas/informacionempleado/informacionempleado.component.ts
+++ src/app/paginas/informacionempleado/informacionempleado.component.ts
@@ -5,6 +5,7 @@ import { PersonaEmpleado } from 'src/app/model/PersonaEmpleado.model';
 import { PersonaServiceTsServiceService } from 'src/app/service/persona-service-ts-service.service';
 import { ServiceindumentariaServiceService } from 'src/app/service/serviceindumentaria.service.service';
 
+declare var $: any;
 @Component({
   selector: 'app-informacionempleado',
   templateUrl: './informacionempleado.component.html',
@@ -53,8 +54,11 @@ export class InformacionempleadoComponent {
   monthSelect: any[] = [];
   dateSelect: any;
   dateValue: any;
-
   uniformes: Map<string, Array<Indumentaria>>= new Map();
+
+  indexUniforme1 : any = 0;
+
+  indexUniforme2 :any = 1
 
   constructor(
     private router: Router,
@@ -79,6 +83,15 @@ export class InformacionempleadoComponent {
     //   this.indumentaria = dataI;
     // });
   }
+
+  openModal(id:string) {
+    $(id).modal('show');
+  }
+
+  closeModal(id:string){
+    $(id).modal('hide');
+  }
+
   main() {
     this.router.navigate(['/main']);
   }
@@ -95,4 +108,106 @@ export class InformacionempleadoComponent {
   //     }
   //   });
   // }
+
+  getPropiedades(objeto: any): [string, any][] {
+    return Object.entries(objeto).filter(([key, value]) => key !== 'nombre');
+  }
+
+  uniformesRecibidos = {
+    "uniformes": [
+      {
+        "nombre": "IPO Brigadista",
+        "Pantalon": "38",
+        "Chomba": "M",
+        "Zapatos": "L",
+        "Polar": "XL",
+        "Campera": "M"
+      },
+      {
+        "nombre": "Profile",
+        "Pantalon": "XL",
+        "Chomba": "L",
+        "Zapatos": "M",
+        "Polar": "L",
+        "Campera": "XL"
+      }
+    ]
+  }
+  
+
+
+  uniformesData = {
+    "uniformes": [
+      {
+        "nombre": "UNIFORME VIGILADOR GPS HOMBRE/MUJER",
+        "items": [
+          "CAMISA UNISEX 3 BORDADOS",
+          "PANTALON RECTO",
+          "CORBATA GPS",
+          "CHALECO FLUO Amarillo",
+          "CINTA PORTA CREDENCIAL",
+          "ZAPATOS DE SEGURIDAD",
+          "CAMPERA REFLEX GPS",
+          "SORDINA",
+          "CINTURON"
+        ]
+      },
+      {
+        "nombre": "UNIFORME VIGILADOR GLOBAL HOMBRE/MUJER",
+        "items": [
+          "CAMISA UNISEX 3 BORDADOS GLOBAL",
+          "PANTALON FAJINA",
+          "CHALECO FLUO Amarillo",
+          "CINTA PORTA CREDENCIAL",
+          "CORBATA GLOBAL",
+          "ZAPATOS DE SEGURIDAD",
+          "POLAR GLOBAL",
+          "CAMPERA REFLEX GLOBAL",
+          "CINTURON"
+        ]
+      },
+      {
+        "nombre": "UNIFORMES PROFILER MUJER",
+        "items": [
+          "CAMISA DE VESTIR MUJER 3/4",
+          "PANTALON VESTIR MUJER",
+          "SACOS PROFILERS MUJER",
+          "PAÑUELO",
+          "CHALECO FLUO Amarillo",
+          "CINTA PORTA CREDENCIAL",
+          "ZAPATOS DE SEGURIDAD",
+          "CAMPERA REFLEX GPS"
+        ]
+      },
+      {
+        "nombre": "UNIFORME PROFILER HOMBRE",
+        "items": [
+          "CAMISA DE VESTIR HOMBRE ML",
+          "PANTALON VESTIR HOMBRE",
+          "SACOS PROFILERS HOMBRE",
+          "CORBATA GPS",
+          "CHALECO FLUO Amarillo",
+          "CINTA PORTA CREDENCIAL",
+          "ZAPATOS DE SEGURIDAD",
+          "CAMPERA REFLEX GPS"
+        ]
+      },
+      {
+        "nombre": "lpo",
+        "items": [
+          "CHOMBA BRIGADISTA",
+          "PANTALON FAJINA HOMBRE",
+          "CHALECO FLUO Amarillo",
+          "CINTA PORTA CREDENCIAL",
+          "ZAPATOS DE SEGURIDAD",
+          "CAMPERA REFLEX GPS",
+          "SORDINA & GORRO",
+          "CHALECO FLUO Naranja",
+          "CINTA PORTA CREDENCIAL",
+          "BORCEGOS"
+        ]
+      }
+    ]
+  }
+  
 }
