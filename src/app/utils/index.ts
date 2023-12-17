77f5f6736783f06113c2bcb52589902cab78737f
Matias Basilotta <matiasbasilotta@gmail.com>
2023-12-17 18:38:45 -0300

    fixes

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,22 +1,23 @@
 export function getLogoByGerencia(gerencia: string):string {
     let url = './assets/img/logos/';
+    console.log(gerencia);
     switch (gerencia) {
-        case 'GER_001':
+        case 'GER01':
             url += 'sima.png'
             break;
-        case 'GER_002':
+        case 'GER02':
             url += 'global.png'
             break;
-        case 'GER_003':
+        case 'GER03':
             url += 'global.png'
             break;
-        case 'GER_004':
+        case 'GER04':
             url += 'sima.png'
             break;
-        case 'GER_005':
+        case 'GER05':
             url += 'lb.png'
             break;
-        case 'GER_006':
+        case 'GER06':
             url += 'eco.png'
             break;
         default:
