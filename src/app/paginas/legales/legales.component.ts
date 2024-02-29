8ea6906a20a32dcc78911bcb17feaeabbaa2e537
Manoel Moreira <mmnmoreira@gmail.com>
2024-02-28 23:26:02 -0300

    Agrego el Modulo Legales

---
+++ src/app/paginas/legales/legales.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legales',
+  standalone: true,
+  imports: [],
+  templateUrl: './legales.component.html',
+  styleUrl: './legales.component.css'
+})
+export class LegalesComponent {
+
+}
